e4b31ea9fd5519b9946020ced9934b59a7c0735d
rutu141 <46307957+rutu141@users.noreply.github.com>
2022-07-19 19:23:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+from turtle import Turtle,Screen
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("medium purple")
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+screen=Screen()
+screen.exitonclick()
